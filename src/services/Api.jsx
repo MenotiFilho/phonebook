f153bacde821bd8a07f3e533dfd1fccfbde70bf2
Menoti Filho <menotimfilho@gmail.com>
2023-06-03 17:04:12 -0300

    server

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const addContact = (newContact) => {
 	return axios
-		.post('http://localhost:3001/persons', newContact) // Envia uma requisição POST para adicionar um novo contato
+		.post('https://serverphonebook.vercel.app/persons', newContact) // Envia uma requisição POST para adicionar um novo contato
 		.then((response) => response.data) // Retorna os dados do contato adicionado em caso de sucesso
 		.catch((error) => {
 			throw new Error(`Error adding contact: ${error}`); // Lança um erro com uma mensagem personalizada em caso de erro
@@ -11,7 +11,7 @@ export const addContact = (newContact) => {
 
 export const getContacts = () => {
 	return axios
-		.get('http://localhost:3001/persons') // Envia uma requisição GET para obter a lista de contatos
+		.get('https://serverphonebook.vercel.app/persons') // Envia uma requisição GET para obter a lista de contatos
 		.then((response) => response.data) // Retorna os dados dos contatos em caso de sucesso
 		.catch((error) => {
 			throw new Error(`Error getting contacts: ${error}`); // Lança um erro com uma mensagem personalizada em caso de erro
@@ -20,7 +20,7 @@ export const getContacts = () => {
 
 export const deleteContact = (contactId) => {
 	return axios
-		.delete(`http://localhost:3001/persons/${contactId}`) // Envia uma requisição DELETE para deletar um contato pelo ID
+		.delete(`https://serverphonebook.vercel.app/persons/${contactId}`) // Envia uma requisição DELETE para deletar um contato pelo ID
 		.then((response) => response.data) // Retorna os dados do contato deletado em caso de sucesso
 		.catch((error) => {
 			throw new Error(`Error deleting contact: ${error}`); // Lança um erro com uma mensagem personalizada em caso de erro
@@ -29,7 +29,10 @@ export const deleteContact = (contactId) => {
 
 export const updateContact = (contactId, updatedContact) => {
 	return axios
-		.put(`http://localhost:3001/persons/${contactId}`, updatedContact) // Envia uma requisição PUT para atualizar um contato pelo ID
+		.put(
+			`https://serverphonebook.vercel.app/persons/${contactId}`,
+			updatedContact
+		) // Envia uma requisição PUT para atualizar um contato pelo ID
 		.then((response) => response.data) // Retorna os dados do contato atualizado em caso de sucesso
 		.catch((error) => {
 			throw new Error(`Error updating contact: ${error}`); // Lança um erro com uma mensagem personalizada em caso de erro
