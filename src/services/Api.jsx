817859a1d2cdfe9764891dd5ca1cc3b73127587a
Menoti Filho <menotimfilho@gmail.com>
2023-06-05 15:13:52 -0300

    .

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
+const baseUrl = 'http://localhost:3001/api/persons';
+
 export const addContact = (newContact) => {
 	return axios
-		.post(
-			'https://my-json-server.typicode.com/MenotiFilho/phoneserver/persons',
-			newContact
-		) // Envia uma requisição POST para adicionar um novo contato
+		.post(baseUrl, newContact) // Envia uma requisição POST para adicionar um novo contato
 		.then((response) => response.data) // Retorna os dados do contato adicionado em caso de sucesso
 		.catch((error) => {
 			throw new Error(`Error adding contact: ${error}`); // Lança um erro com uma mensagem personalizada em caso de erro
@@ -14,7 +13,7 @@ export const addContact = (newContact) => {
 
 export const getContacts = () => {
 	return axios
-		.get('https://my-json-server.typicode.com/MenotiFilho/phoneserver/persons') // Envia uma requisição GET para obter a lista de contatos
+		.get(baseUrl) // Envia uma requisição GET para obter a lista de contatos
 		.then((response) => response.data) // Retorna os dados dos contatos em caso de sucesso
 		.catch((error) => {
 			throw new Error(`Error getting contacts: ${error}`); // Lança um erro com uma mensagem personalizada em caso de erro
@@ -23,9 +22,7 @@ export const getContacts = () => {
 
 export const deleteContact = (contactId) => {
 	return axios
-		.delete(
-			`https://my-json-server.typicode.com/MenotiFilho/phoneserver/persons/${contactId}`
-		) // Envia uma requisição DELETE para deletar um contato pelo ID
+		.delete(`${baseUrl}/${contactId}`) // Envia uma requisição DELETE para deletar um contato pelo ID
 		.then((response) => response.data) // Retorna os dados do contato deletado em caso de sucesso
 		.catch((error) => {
 			throw new Error(`Error deleting contact: ${error}`); // Lança um erro com uma mensagem personalizada em caso de erro
@@ -34,10 +31,7 @@ export const deleteContact = (contactId) => {
 
 export const updateContact = (contactId, updatedContact) => {
 	return axios
-		.put(
-			`https://my-json-server.typicode.com/MenotiFilho/phoneserver/persons/${contactId}`,
-			updatedContact
-		) // Envia uma requisição PUT para atualizar um contato pelo ID
+		.put(`${baseUrl}/${contactId}`, updatedContact) // Envia uma requisição PUT para atualizar um contato pelo ID
 		.then((response) => response.data) // Retorna os dados do contato atualizado em caso de sucesso
 		.catch((error) => {
 			throw new Error(`Error updating contact: ${error}`); // Lança um erro com uma mensagem personalizada em caso de erro
